ada9fe5a0b4ee2e3ce12c39ba186ed8e6244899a
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2019-12-08 16:33:40 +0800

    project overview 1.1

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+dhvsuProject = document.getElementById("dhvsuProject");
+
+
+
+function show() {
+    dhvsuProject.style.display = "block";
+}
+
+function hide() {
+    dhvsuProject.style.display = "none";
+}
