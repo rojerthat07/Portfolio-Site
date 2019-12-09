7c60243a26724037a6e964adebe5d2e4abf58a93
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2019-12-09 12:42:44 +0800

    project 123

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,39 +1,82 @@
-dhvsuProject = document.getElementById("dhvsuProject");
-navigation = document.getElementById("navigation");
 
-function show() {
-    dhvsuProject.style.display = "block";
-
+//PROJECT OVERVIEW SHOW AND HIDE
+function show(id) {
+    document.getElementById(id).style.display = "block";
 }
-
-function hide() {
-    dhvsuProject.style.display = "none";
+function hide(id) {
+    document.getElementById(id).style.display = "none";
 }
 
 
 //Scroll Magic and TweenMax
-const tl = new TimelineMax();
-const tl2 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
-
-tl.from("#project__overview--introduction", .5, {
+//DHVSU OVERVIEW
+const dhvsutl = new TimelineMax();
+const dhvsutl2 = new TimelineMax();
+//Effects
+dhvsutl.from("#project-overview-dhvsu", .5, {
     opacity: 1
 });
-tl2.from("#project__overview--images", 2, {
+dhvsutl2.from("#project-overview-dhvsu-images", 2, {
     opacity: 0,
-    y: -50
+    y: -100
 });
+//Trigger Scenes
+const dhvsuScene1 = new ScrollMagic.Scene({
+    triggerElement: "#project-overview-dhvsu-trigger"
+})
+    .setTween(dhvsutl)
+    .addTo(controller)
+const dhvsuScene2 = new ScrollMagic.Scene({
+    triggerElement: "#project-overview-dhvsu-image-trigger"
+})
+    .setTween(dhvsutl2)
+    .addTo(controller)
+//UNIVERSE OVERVIEW
+const universetl = new TimelineMax();
+const universetl2 = new TimelineMax();
+//Effects
+universetl.from("#project-overview-universe", .5, {
+    opacity: 1
+});
+universetl2.from("#project-overview-universe-images", 2, {
+    opacity: 0,
+    y: -100
+});
+//Trigger Scenes
+const universeScene1 = new ScrollMagic.Scene({
+    triggerElement: "#project-overview-universe-trigger"
+})
+    .setTween(universetl)
+    .addTo(controller)
+const universeScene2 = new ScrollMagic.Scene({
+    triggerElement: "#project-overview-universe-image-trigger"
+})
+    .setTween(universetl2)
+    .addTo(controller)
+//GAMING PC OVERVIEW
+const gamingPctl = new TimelineMax();
+const gamingPctl2 = new TimelineMax();
+//Effects
+gamingPctl.from("#project-overview-gaming-pc", .5, {
+    opacity: 1
+});
+gamingPctl2.from("#project-overview-gaming-pc-images", 2, {
+    opacity: 0,
+    y: -100
+});
+//Trigger Scenes
+const gamingPc1 = new ScrollMagic.Scene({
+    triggerElement: "#project-overview-gaming-pc-trigger"
+})
+    .setTween(gamingPctl)
+    .addTo(controller)
+const gamingPcScene2 = new ScrollMagic.Scene({
+    triggerElement: "#project-overview-gaming-pc-images-trigger"
+})
+    .setTween(gamingPctl2)
+    .addTo(controller)
 
-const scene1 = new ScrollMagic.Scene({
-    triggerElement: "#project__overview--introduction-trigger"
-})
-    .setTween(tl)
-    .addTo(controller)
-const scene2 = new ScrollMagic.Scene({
-    triggerElement: "#project__overview--image-trigger"
-})
-    .setTween(tl2)
-    .addTo(controller)
 
 
 
