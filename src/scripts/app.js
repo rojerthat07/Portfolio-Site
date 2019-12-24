d0f1f5c57640965495f20ce72b843acc570e8594
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2019-12-24 16:37:40 +0800

    type writer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,6 +6,83 @@ function hide(id) {
     document.getElementById(id).style.display = "none";
 }
 
+//Typing Effect
+const TypeWriter = function(txtElement,words,wait = 5000){
+    this.txtElement = txtElement;
+    this.words = words; 
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait,10);
+    this.type();
+    this.isDeleting = false;
+}
+
+    //Type Method
+    TypeWriter.prototype.type = function(){
+        //Current index of word
+        const current = this.wordIndex % this.words.length;
+        //Get full text of current
+        const fullTxt = this.words[current];
+
+        if(this.isDeleting){
+            //Remove Char
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            //Add Char
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+
+        //Insert txt into element
+        this.txtElement.innerHTML = `<span class="pointer">${this.txt}</span>`;
+
+        //Initial Type Speed
+        typeSpeed = 300;
+
+        if(this.isDeleting){
+            typeSpeed /= 2;
+        }
+
+        if(!this.isDeleting && this.txt === fullTxt){
+            //Makes pause at end
+            typeSpeed = this.wait;
+            if( fullTxt == this.words[0]){
+                document.querySelector(".contact-txt").innerHTML += ".";
+            }
+            if( fullTxt == this.words[1]){
+                document.querySelector(".contact-txt").innerHTML += "..";
+            }
+            if( fullTxt == this.words[2]){
+                document.querySelector(".contact-txt").innerHTML += "...";
+            }
+           
+            document.querySelector(".pointer").style.animationName = "pointer-blink-stop";
+            //Set delete to true
+            this.isDeleting = true;
+        }else if(this.isDeleting && this.txt === ''){
+            this.isDeleting = false;
+              // Move to next word
+      this.wordIndex++;
+            this.typeSpeed = 500;
+        }
+
+ 
+        setTimeout(()=> this.type(), typeSpeed)
+    }
+
+
+   
+    //Init on DOM Load
+document.addEventListener("DOMContentLoaded",init);
+    //Init App
+function init(){
+    const txtElement = document.querySelector(".contact-txt");
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    //Initialize Typewriter
+    new TypeWriter(txtElement,words,wait)
+}
+
 
 
 //Scroll Magic and TweenMax
